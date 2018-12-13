f87ecd346bbbf2c95e23a24f8084d2dbe5692580
Scott Waring <scott.waring@gmail.com>
2018-12-13 14:57:14 -0500

    add burger menu

--- play-found-frontend/src/Components/navBar.js
+++ play-found-frontend/src/Components/navBar.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
+import { slide as Menu } from 'react-burger-menu'
 
 class NavBar extends Component {
+  state = {
+    isOpen: false
+  }
+  showSettings (event) {
+     event.preventDefault();
+     this.setState({isOpen: true})
+  }
 
   render() {
     return (
       <div className="nav-header">
+      <Menu width={'30%'} >
+        <a id="home" className="menu-item" href="/">Home</a>
+        <a id="about" className="menu-item" href="/about">About</a>
+        <a id="contact" className="menu-item" href="/contact">Contact</a>
+      </Menu>
         <div className="nav-drop-menu">
-          <img className="drop-menu" alt=" " src={require("../assets/transparent-drop-menu.png")}/>
+          <img onClick={ this.showSettings } className="drop-menu" alt=" " src={require("../assets/transparent-drop-menu.png")}/>
         </div>
         <div className="castle-header">
           <img className="castle" alt=" " src={require("../assets/full-transparent-title-logo.png")}/>
