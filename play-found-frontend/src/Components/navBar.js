5a25640a8803d4880e6117ad03b2cc89f9a6ab75
Scott Waring <scott.waring@gmail.com>
2018-12-13 11:57:07 -0500

    initial frontend scaffolding

---
+++ play-found-frontend/src/Components/navBar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class NavBar extends Component {
+
+  render() {
+    return (
+      <div>
+        HelloWorlds!
+      </div>
+    )
+  }
+}
+
+export default NavBar
---
+++ play-found-frontend/src/Reducers/reducers.js
@@ -0,0 +1,14 @@
+
+let initalState = {
+  user: {},
+  playgrounds: [],
+  selectedPlayground: {},
+  bathrooms: [],
+  reviews: []
+}
+
+
+
+export default function reducers(state = initalState, action) {
+  return state
+}
